592554afe1f62e27db36a57013b15517462320e1
szoradigeza <szoradigeza@gmail.com>
2018-11-15 23:38:41 +0100

    LoginData class for json file

---
+++ client/HomeSpeakClient/DesktopApp1/LoginData.cs
@@ -0,0 +1,28 @@
+﻿
+public class LoginData
+{
+    private static LoginData instance = null;
+
+    public int type { get; set; }
+    public int id { get; set; }
+    public string username { get; set; }
+    public string token { get; set; }
+    public string email { get; set; }
+    public string password { get; set; }
+
+    private LoginData()
+    {
+    }
+    public static LoginData Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new LoginData();
+            }
+            return instance;
+        }
+    }
+}
+
