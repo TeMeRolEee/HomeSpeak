ec3e047f5d53b3d381e66d52f7bd45d57964e4c4
szilardvathy <szilardvathy@gmail.com>
2018-12-12 00:17:22 +0100

    Getting automatically the users IP

--- client/HomeSpeakClient/DesktopApp1/IpconnectionData.cs
+++ client/HomeSpeakClient/DesktopApp1/IpconnectionData.cs
@@ -4,27 +4,42 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
+class IpconnectionData
+{
 
-    class IpconnectionData
+    public int portNumber = 8086;
+    public string hostName = "";
+    public TcpClient client;
+    private NetworkStream networkStream;
+    private static IpconnectionData instance = null;
+    public static IpconnectionData Instance
     {
-
-        public int portNumber = 8086;
-        public string hostName = "172.21.29.106";
-        public TcpClient client;
-        private NetworkStream networkStream;
-        private static IpconnectionData instance = null;
-        public static IpconnectionData Instance
+        get
         {
-            get
+            if (instance == null)
             {
-                if (instance == null)
-                {
-                    instance = new IpconnectionData();
-                }
-                return instance;
+                instance = new IpconnectionData();
+            }
+            return instance;
+        }
+    }
+
+    /* well fuck.? for future use */
+    public static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                Console.WriteLine("{0}", ip.ToString());
+                return ip.ToString();
             }
         }
-
-        public NetworkStream NetworkStream { get => networkStream; set => networkStream = value; }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
     }
+
+    public NetworkStream NetworkStream { get => networkStream; set => networkStream = value; }
+}
